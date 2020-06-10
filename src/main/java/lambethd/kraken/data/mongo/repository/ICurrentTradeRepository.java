39315cb8cd0d8430f841aad2a778c2d5c89a6a48
David Lambeth <dl561@bath.ac.uk>
2020-06-10 13:49:02 +0100

    General commit

--- src/main/java/lambethd/kraken/data/mongo/repository/ICurrentTradeRepository.java
+++ src/main/java/lambethd/kraken/data/mongo/repository/ICurrentTradeRepository.java
@@ -4,11 +4,11 @@ import org.bson.types.ObjectId;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import portfolio.HistoricalTrade;
+import portfolio.Trade;
 
 import java.util.List;
 
-public interface ITradeRepository extends MongoRepository<HistoricalTrade, String> {
-    HistoricalTrade findTradeById(ObjectId tradeId);
-
-    List<HistoricalTrade> findTradeByUsername(String username, Sort sort);
+public interface ICurrentTradeRepository extends MongoRepository<Trade, String> {
+    Trade findTradeById(ObjectId tradeId);
+    List<Trade> findTradeByUsername(String username, Sort sort);
 }
