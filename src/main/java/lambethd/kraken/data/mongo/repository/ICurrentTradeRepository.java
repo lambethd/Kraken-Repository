c8c523832d850654c66ae49d4f067980e7ce5e5e
David Lambeth <david.lambeth@outlook.com>
2020-07-03 08:54:50 +0100

    Changed findTradeById to String

--- src/main/java/lambethd/kraken/data/mongo/repository/ICurrentTradeRepository.java
+++ src/main/java/lambethd/kraken/data/mongo/repository/ICurrentTradeRepository.java
@@ -1,14 +1,12 @@
 package lambethd.kraken.data.mongo.repository;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import portfolio.HistoricalTrade;
 import portfolio.Trade;
 
 import java.util.List;
 
 public interface ICurrentTradeRepository extends MongoRepository<Trade, String> {
-    Trade findTradeById(ObjectId tradeId);
+    Trade findTradeById(String tradeId);
     List<Trade> findTradeByUsername(String username, Sort sort);
 }
